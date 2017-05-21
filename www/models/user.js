e08346f9717c42da58d8cc3179046bd419c623f3
titus <titus@dump-titus.42.fr>
2017-05-21 18:38:22 +0200

    login page

--- www/models/user.js
+++ www/models/user.js
@@ -2,50 +2,68 @@ let connection = require('../config/db')
 
 class User {
 
-	constructor (row) {
-		this.row = row
-	}
-	get email() {
-		return this.row.email
-	}
+    constructor(row) {
+        this.row = row
+    }
+    get email() {
+        return this.row.email
+    }
 
-	static create (content, callback) {
-		connection.query('SELECT email FROM users WHERE email = ?', [content.email], (err, result) => {
-			if (err) throw err
-			if (result.length > 0)
-			{
-				callback("taken")
-			} else {
-				connection.query('INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?);',
-					[content.firstName, content.lastName, content.email, content.password],
-					function(err, result) {
-						if (err) throw err
-						callback('success')
-				})
-			}
-		})
-	}
-	static find (content, callback) {
-		connection.query('SELECT * FROM users WHERE id = ? LIMIT 1', [content], (err, rows) => {
-			if (err) throw err
-			callback(new User(rows[0]))
-		})
-	}
-	static all (callback) {
-		connection.query('SELECT * FROM users', (err, rows) => {
-			if (err) throw err
-			callback(rows.map((row) => new User(row)))
-		})
-	}
-	static is_complete (id, callback) {
-		connection.query('SELECT validate_step FROM users WHERE id = ?', [id], (err, rows) => {
-			if (err) throw err
-			if (rows[0].validate_step === 0)
-				callback(true)
-			else
-				callback(false)
-		})
-	}
+    static create(content, callback) {
+        connection.query('SELECT email FROM users WHERE email = ?', [content.email], (err, result) => {
+            if (err) throw err
+            if (result.length > 0) {
+                callback("taken")
+            } else {
+                let bcrypt = require('bcrypt');
+                const saltRounds = 10;
+                bcrypt.hash(content.password, saltRounds, function(err, hash) {
+                    if (err) throw err
+                    connection.query('INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?);', [content.firstName, content.lastName, content.email, hash],
+                        function(err, result) {
+                            if (err) throw err
+                            callback('success')
+                        })
+                })
+            }
+        })
+    }
+    static find(content, callback) {
+        connection.query('SELECT * FROM users WHERE id = ? LIMIT 1', [content], (err, rows) => {
+            if (err) throw err
+            callback(new User(rows[0]))
+        })
+    }
+    static all(callback) {
+        connection.query('SELECT * FROM users', (err, rows) => {
+            if (err) throw err
+            callback(rows.map((row) => new User(row)))
+        })
+    }
+
+    static sign_in(content, callback) {
+    	connection.query('SELECT email, password FROM users WHERE email = ? LIMIT 1', [content.email], (err, rows) => {
+    		if (err) throw err
+    		if (rows.length) {
+    			let bcrypt = require('bcrypt');
+    			bcrypt.compare(content.password, rows[0].password, (err, res) => {
+    				if (err) throw err
+    				callback(res ? true : "Invalid password");
+    			})
+    		} else
+    			callback("Unknown e-mail")
+    	})
+    }
+
+    static is_complete(id, callback) {
+        connection.query('SELECT validate_step FROM users WHERE id = ?', [id], (err, rows) => {
+            if (err) throw err
+            if (rows[0].validate_step === 0)
+                callback(true)
+            else
+                callback(false)
+        })
+    }
 }
 
 module.exports = User
--- www/routes/index.js
+++ www/routes/index.js
@@ -2,53 +2,51 @@ let fs = require('fs');
 
 module.exports = (app) => {
 
-  app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('welcome here')
-  })
-
-  app.get('/test', (req, res) => {
-    res.render('test')
-  })
-
-  // ---- USERS -----
-  app.get('/users/register', (req, res) => {
-    res.render('users/register')
-  })
-  app.post('/users/register', (req, res) => {
-    let Checker = require('../models/checker')
-    let User = require('../models/user')
-    Checker.register(req.body, (callback) => {
-      if (callback !== 'ok') {
-        req.flash('error', callback)
-        res.redirect('register')
-      } else {
-        User.create(req.body, (callback) => {
-          if (callback === 'success')
-            req.flash('success', "Welcome to matcha site!")
-          res.redirect('register')  
+    // ---- USERS -----
+    app.get('/register', (req, res) => {
+        res.render('users/register')
+    })
+    app.post('/register', (req, res) => {
+        let Checker = require('../models/checker')
+        let User = require('../models/user')
+        Checker.register(req.body, (callback) => {
+            if (callback !== 'ok') {
+                req.flash('error', callback)
+                res.redirect('register')
+            } else {
+                User.create(req.body, (callback) => {
+                    if (callback === 'success')
+                        req.flash('success', "Welcome to matcha site!")
+                    res.redirect('register')
+                })
+            }
         })
-      }
     })
-  })
-app.get('/users/:id', (req, res) => {
-  let id = req.params.id;
-  let User = require('../models/user')
-  User.is_complete(id, (validate) => {
-    User.find(id, (user) => {
-      res.render('users/index', {user: user, validate: validate})
+    app.get('/users/:id', (req, res) => {
+        let id = req.params.id;
+        let User = require('../models/user')
+        User.is_complete(id, (validate) => {
+            User.find(id, (user) => {
+                res.render('users/index', {
+                    user: user,
+                    validate: validate
+                })
+            })
+        })
     })
-  })
-})
-  // app.get('/users', (req, res) => {
-  //   let User = require('../models/user')
-  //   User.all(function (users) {
-  //     res.render('users/index', {users: users})  
-  //   })
-  // })
-
-  app.get('/users/login', (req, res) => {
-    res.render('users/login')
-  })
+    app.get('/login', (req, res) => {
+        res.render('users/login')
+    })
+    app.post('/login', (req, res) => {
+      let User = require('../models/user')
+      User.sign_in(req.body, (callback) => {
+        if (callback !== true) {
+          req.flash('error', callback)
+          res.redirect('login')
+        } else {
+          
+        }
+      })
+    })
 
 }
